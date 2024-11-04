379eb92f75b4a60ab5d9665f78c82d669a4663bd
LuCAires <lucas.l.aires@gmail.com>
2024-11-04 15:57:15 -0300

    feat: new repo

---
+++ airflow_pipeline/dags/twitter_dag.py
@@ -0,0 +1,27 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from airflow.models import DAG
+from datetime import datetime, timedelta
+from operators.twitter_operator import TwitterOperator
+from airflow.utils.dates import days_ago
+
+with DAG(
+        dag_id="TwitterDag",
+        start_date=days_ago(2), 
+        schedule="@daily"
+    ) as dag:
+        TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
+        query = "data science"
+        
+        to = TwitterOperator(
+            file_path=os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'airflow_pipeline', 'datalake', 'twitter_datascience')),
+                           "extract_date={{ ds }}",
+                           "datascience_{{ ds_nodash }}.json"),                       
+            query=query, 
+            start_time="{{ data_interval_start.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}", 
+            end_time="{{ data_interval_end.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}", 
+            task_id="twitter_datascience"
+            )
+        
